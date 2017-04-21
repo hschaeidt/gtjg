a70714c30df01e870f8e36f000d017d6248dcab5
Hendrik Schaeidt <he.schaeidt@gmail.com>
2017-04-21 12:16:05 +0200

    add production build config

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,4 +1,14 @@
-const merge = require("webpack-merge");
-const baseConfig = require("./webpack.config.js");
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.js');
 
-module.exports = merge({ }, baseConfig);
+module.exports = merge({
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
+}, baseConfig);
