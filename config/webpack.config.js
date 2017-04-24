92b8c7b2134530079d93bd91c442aa9c6bdea862
Hendrik Schaeidt <he.schaeidt@gmail.com>
2017-04-24 18:28:56 +0200

    webpack: fix entrypoint reference
    this bug was introduced with the renaming in commit
    80b2402b6078591cdfb6b9e5f38ec7cd90ab0219

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/app.tsx",
+  entry: "./src/App.tsx",
   output: {
     path: path.join(__dirname, '..', 'public'),
     filename: 'app.js',
